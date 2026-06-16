270e84d11b29beb9bbe623364019d178d86b52e2
Shresth500 <shresthchomal@gmail.com>
2026-06-16 19:33:56 +0530

    Removed some bugs

--- services/chat_bot_service.py
+++ services/chat_bot_service.py
@@ -1,3 +1,5 @@
+import logging
+
 from mem0 import Memory
 from langchain_ollama import ChatOllama
 
@@ -29,8 +31,10 @@ config = {
 
 memory_client = Memory.from_config(config)
 llm = ChatOllama(model="llama3", temperature=0.1)
+logger = logging.getLogger(__name__)
 
 def chat_app(user_query:str, user_id:int, chat_window_id:int):
+    logger.info(f"Getting the contents from the mem0")
     memories = memory_client.search(
                         query=user_query,
                         filters={
@@ -39,21 +43,25 @@ def chat_app(user_query:str, user_id:int, chat_window_id:int):
                         },
                         limit=5
                     )
-
-    memory_context = "\n\n".join(memory for memory in memories)
+    logger.info(f"Previous Messages close to the User-query:{memories}")
+    memory_context = "\n\n".join(memory["memory"] for memory in memories.get("results", []))
     prompt = f"""
         You are an helpful assistant 
         Previous context: 
         {memory_context}
     """
-
+    logger.info(f"Loading the response from the LLM")
     response = llm.invoke(prompt)
 
+    logger.info(f"Got the Response from the LLM")
+    logger.info(f"Loading the conversation inside the mem0")
     memory_client.add(
         messages=[
             {"role":"user","content":user_query},
             {"role":"assistant","content":response.content}
-        ]
+        ],
+        user_id=str(user_id),
+        run_id=str(chat_window_id)
     )
     return ChatBotResponse(
         status="success",
